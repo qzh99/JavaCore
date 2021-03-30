5ebc71f3984a2c6621fc7449998021aec4c4e0dc
qinzhenghua <1393950470@qq.com>
2021-03-30 22:10:42 +0800

    enum test

--- src/me/qzh/enums/Test.java
+++ src/me/qzh/enums/Test.java
@@ -11,9 +11,23 @@ import java.util.Arrays;
 public class Test {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(Subject.values()));
-
         scan:{
 
         }
+    }
+
+    @org.junit.Test
+    public void test(){
+        System.out.println(Subject.ENGLISH.compareTo(Subject.MATH));
+        System.out.println(Subject.ENGLISH.equals(Week.SUN));
+
+        System.out.println(Subject.ENGLISH.getDeclaringClass());
+
+        System.out.println(Week.MON.name());
+        System.out.println(Week.MON.toString());
+        System.out.println(Week.MON.ordinal());
+
+
+        System.out.println(Enum.valueOf(Week.class, "SUN"));
     }
 }
---
+++ src/me/qzh/enums/Week.java
@@ -0,0 +1,17 @@
+package me.qzh.enums;
+
+/**
+ * @author qinzhenghua
+ * @version 0.0.1
+ * @since 2021/3/30 21:58
+ */
+public enum Week {
+    /**
+     * 星期天
+     */
+    SUN,
+    /**
+     * 星期一
+     */
+    MON;
+}
